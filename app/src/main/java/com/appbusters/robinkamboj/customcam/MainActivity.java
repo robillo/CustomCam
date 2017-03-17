0449592fffdb6f25cffddaa64487173b350db953
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-03-17 16:07:15 +0530

    manifest

--- app/src/main/java/com/appbusters/robinkamboj/customcam/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/customcam/MainActivity.java
@@ -1,13 +1,17 @@
 package com.appbusters.robinkamboj.customcam;
 
+import android.hardware.camera2.CameraCaptureSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CameraCaptureSession captureSession;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
